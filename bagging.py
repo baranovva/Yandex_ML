2b822a59d09d6c66a2b1f44a80655fb69d97379f
baranovva <144531586+baranovva@users.noreply.github.com>
2023-12-13 16:21:48 +0300

    Add files via upload

--- bagging.py
+++ bagging.py
@@ -1,10 +1,11 @@
 import numpy as np
 
+
 class SimplifiedBaggingRegressor:
     def __init__(self, num_bags, oob=False):
         self.num_bags = num_bags
         self.oob = oob
-        
+
     def _generate_splits(self, data: np.ndarray):
         '''
         Generate indices for every bag and store in self.indices_list list
@@ -13,14 +14,15 @@ class SimplifiedBaggingRegressor:
         data_length = len(data)
         for bag in range(self.num_bags):
             # Your Code Here
-        
+            self.indices_list.append(np.random.choice(data_length, size=data_length, replace=True))
+
     def fit(self, model_constructor, data, target):
         '''
         Fit model on every bag.
         Model constructor with no parameters (and with no ()) is passed to this function.
-        
+
         example:
-        
+
         bagging_regressor = SimplifiedBaggingRegressor(num_bags=10, oob=True)
         bagging_regressor.fit(LinearRegression, X, y)
         '''
@@ -28,44 +30,57 @@ class SimplifiedBaggingRegressor:
         self.target = None
         self._generate_splits(data)
         assert len(set(list(map(len, self.indices_list)))) == 1, 'All bags should be of the same length!'
-        assert list(map(len, self.indices_list))[0] == len(data), 'All bags should contain `len(data)` number of elements!'
+        assert list(map(len, self.indices_list))[0] == len(
+                data), 'All bags should contain `len(data)` number of elements!'
         self.models_list = []
         for bag in range(self.num_bags):
             model = model_constructor()
-            data_bag, target_bag = # Your Code Here
-            self.models_list.append(model.fit(data_bag, target_bag)) # store fitted models here
+            data_bag, target_bag = data[self.indices_list[bag]], target[self.indices_list[bag]]  # Your Code Here
+            self.models_list.append(model.fit(data_bag, target_bag))  # store fitted models here
         if self.oob:
             self.data = data
             self.target = target
-        
+
     def predict(self, data):
         '''
         Get average prediction for every object from passed dataset
         '''
         # Your code here
-    
+        l = []
+        for bag in range(self.num_bags):
+            l.append(self.models_list[bag].predict(data))
+        l = np.asarray(l).T
+        return np.mean(l, axis=1)
+
     def _get_oob_predictions_from_every_model(self):
         '''
         Generates list of lists, where list i contains predictions for self.data[i] object
         from all models, which have not seen this object during training phase
         '''
         list_of_predictions_lists = [[] for _ in range(len(self.data))]
-        # Your Code Here
-        
-        self.list_of_predictions_lists = np.array(list_of_predictions_lists, dtype=object)
-    
+        for i, obj in enumerate(self.data):
+            for model, inxs in zip(self.models_list, self.indices_list):
+                if i not in inxs:
+                    prediction = model.predict([obj])
+                    list_of_predictions_lists[i].append(prediction)
+
+        self.list_of_predictions_lists = np.array(list_of_predictions_lists, dtype="object")
+
     def _get_averaged_oob_predictions(self):
         '''
         Compute average prediction for every object from training set.
         If object has been used in all bags on training phase, return None instead of prediction
         '''
         self._get_oob_predictions_from_every_model()
-        self.oob_predictions = # Your Code Here
-        
-        
+        self.oob_predictions = np.array(
+                [np.mean(pred) if len(pred) > 0 else None for pred in self.list_of_predictions_lists])
+
     def OOB_score(self):
         '''
         Compute mean square error for all objects, which have at least one prediction
         '''
         self._get_averaged_oob_predictions()
-        return # Your Code Here
+        preds_inxs = [i for i, pred in enumerate(self.oob_predictions) if pred is not None]
+        y_true = self.target[preds_inxs]
+        predictions = self.oob_predictions[preds_inxs]
+        return np.mean((y_true - predictions) ** 2)
---
+++ power_iteration.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def get_dominant_eigenvalue_and_eigenvector(data, num_steps):
+    """
+    data: np.ndarray – symmetric diagonalizable real-valued matrix
+    num_steps: int – number of power method steps
+
+    Returns:
+    eigenvalue: float – dominant eigenvalue estimation after `num_steps` steps
+    eigenvector: np.ndarray – corresponding eigenvector estimation
+    """
+    b_k = np.random.rand(data.shape[1])
+
+    for _ in range(num_steps):
+        # calculate the matrix-by-vector product Ab
+        b_k1 = np.dot(data, b_k)
+
+        # calculate the norm
+        b_k1_norm = np.linalg.norm(b_k1)
+
+        # re normalize the vector
+        b_k = b_k1 / b_k1_norm
+
+    return float(b_k.dot(data).dot(b_k.T)), b_k
